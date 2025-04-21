5df5175686d4e8aec7c764a7858f93f253073ed2
Donghyun <hdh1324@gmail.com>
2025-04-21 11:16:33 +0300

    modify fprintf_bonus

---
+++ ft_printf_bonus_bu/libft/ft_lstclear_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donheo <donheo@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 16:22:18 by donheo            #+#    #+#             */
+/*   Updated: 2025/04/18 15:16:22 by donheo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_bonus.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*cur;
+
+	while (*lst)
+	{
+		cur = *lst;
+		*lst = (*lst)->next;
+		ft_lstdelone(cur, del);
+	}
+	*lst = NULL;
+}
---
+++ ft_printf_bonus_bu/libft/ft_lstlast_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donheo <donheo@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 09:59:05 by donheo            #+#    #+#             */
+/*   Updated: 2025/04/18 15:16:32 by donheo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_bonus.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
