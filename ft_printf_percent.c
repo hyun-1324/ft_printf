cead532bb8c1a5d8fd3bbe13d329d3d0e01ff16f
Donghyun <hdh1324@gmail.com>
2025-05-22 09:37:55 +0300

    refactor: simplify print_percent function

--- ft_printf_percent.c
+++ ft_printf_percent.c
@@ -6,7 +6,7 @@
 /*   By: donheo <donheo@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 09:30:46 by donheo            #+#    #+#             */
-/*   Updated: 2025/05/22 09:27:09 by donheo           ###   ########.fr       */
+/*   Updated: 2025/05/22 09:36:05 by donheo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,6 @@ int	print_percent(t_info *info)
 {
 	int	printed_bytes;
 
-	printed_bytes = 0;
-	if (info->minus > -1)
-	{
-		write(1, "%", 1);
-		printed_bytes++;
-	}
-	else
-	{
-		write(1, "%", 1);
-		printed_bytes++;
-	}
+	printed_bytes = write(1, "%", 1);
 	return (printed_bytes);
 }
